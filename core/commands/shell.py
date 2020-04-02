f84e1cfb6b42d37c54e8715e2f92a24e03810553
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-02 20:51:04 +0300

    Update shell.py

--- core/commands/shell.py
+++ core/commands/shell.py
@@ -18,7 +18,7 @@
 #        You should have received a copy of the GNU General Public License
 #        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-DESCRIPTION = "Open zombie's CMD shell."
+DESCRIPTION = "Open zombie CMD shell."
 
 def autocomplete(shell, line, text, state):
     if len(line.split()) > 1:
