98279381c82c7bcd522d2631cd1fb86abe90b583
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-02 20:59:58 +0300

    Update exit.py

--- core/commands/exit.py
+++ core/commands/exit.py
@@ -18,8 +18,6 @@
 #        You should have received a copy of the GNU General Public License
 #        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import time
-
 DESCRIPTION = "Exit Proton Framework."
 
 def autocomplete(shell, line, text, state):
