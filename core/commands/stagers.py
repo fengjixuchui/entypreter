3fe49984b8f7c4e8c18dcb79f128a7a32bfa20a3
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-17 20:22:57 +0300

    Update stagers.py

--- core/commands/stagers.py
+++ core/commands/stagers.py
@@ -113,7 +113,7 @@ def kill_listener(shell, id):
                         old_prompt = shell.prompt
                         old_clean_prompt = shell.clean_prompt
                         readline.set_completer(None)
-                        shell.prompt = '\033[1;77m'+'[?]'+'\033[0m'+' Continue? (y/N): '
+                        shell.prompt = '\033[1;77m'+'[?]'+'\033[0m'+' Continue? (y/n): '
                         shell.clean_prompt = shell.prompt
                         option = shell.get_command(shell.prompt)
 
